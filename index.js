beda004f2eb53b2ce0450f7d456132e1ce8b93a5
Shamir Parker <SDPB18@MacBook-Pro-de-Shamir.local>
2018-04-24 22:14:03 -0500

    Se agrega cadena para agregar a las replicas

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./app');
 var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/proyUTP',
+mongoose.connect('mongodb://192.168.13.139:27017,192.168.13.138:27017,192.168.13.138:27017/rastifyApp?replicaSet=rty0',
 (err,res) => {
   if(err){
     throw err;
